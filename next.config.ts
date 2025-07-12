4d320ca5063bd8e6ad1a6960e97a65d6b091430a
MohammedYaseenRon <yaseenron3@gmail.com>
2025-07-12 22:51:10 +0530

    Final Init

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    domains: ['randomuser.me', 'unsplash.com']
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        pathname: '/api/portraits/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
